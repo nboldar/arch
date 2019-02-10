e3d9e53adcc025aa82a758a794852708f5f8bad9
nboldar <nboldar1@gmail.com>
2019-02-10 18:35:52 +0300

    pattern Adapter wasimplemented for db_connection libraries

---
+++ src/Component/Adapter/DbAdapter/MysqlAdapter.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nik
+ * Date: 09.02.2019
+ * Time: 17:13
+ */
+
+namespace Component\Adapter\DbAdapter;
+
+
+use Eden\Mysql\Index;
+
+class MysqlAdapter implements IDbAdapter
+{
+    private $dbConnection;
+
+    /**
+     * MysqlAdapter constructor.
+     */
+    public function __construct (Index $indexObj)
+    {
+        $this->dbConnection = $indexObj;
+    }
+
+    public function insert (string $table, array $settings)
+    {
+        $this->dbConnection->insertRow($table, $settings);
+    }
+
+    public function update (string $table, array $condition, array $settings)
+    {
+        $this->dbConnection->updateRows($table, $condition, $settings);
+    }
+
+
+    public function delete (string $table, int $id)
+    {
+        $filter = ['id=%s', $id];
+        $this->dbConnection->deleteRows($table, $filter);
+    }
+
+    public function getOne (string $table, int $id)
+    {
+        return $this->dbConnection->getRow($table, 'id', $id);
+    }
+
+    public function getAll (string $table, string $columnName, $value)
+    {
+        return $this->dbConnection->getRow($table, $columnName, $value);
+    }
+
+}
